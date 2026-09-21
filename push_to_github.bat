095f25489a283b1a04f97de60dc98380fd84eb29
tamimhasan1280 <tamimhasan1281@gmail.com>
2026-09-22 02:24:48 +0600

    chore: update push banner

--- push_to_github.bat
+++ push_to_github.bat
@@ -1,6 +1,6 @@
 @echo off
 echo =======================================================
-echo   Pushing to Public Repo: tpanel
+echo   Pushing to Public Client Repo: TPANEL-VPS-SETUP
 echo =======================================================
 echo.
 git push -u origin main
